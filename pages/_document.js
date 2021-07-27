892045fe3afdf565a09267aee6d280fb29ad307a
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2021-07-27 23:39:11 +0545

    made pwa compatable

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+        <Html>
+            <Head>
+                <link rel="manifest" href="/manifest.json"/>
+                <link rel="apple-touch-icon" href="/icon512x512.png" />
+                <meta name="theme-color" content="#131331"/>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}}
+export default MyDocument
